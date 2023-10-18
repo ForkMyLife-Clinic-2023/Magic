506b3dbd9fe136ebcb2ec5ec8f3fc9721a6651f3
HanBinGer <35628064+HanBinGer@users.noreply.github.com>
2023-10-18 16:32:56 +0300

    change tgAPI framework and move token from code to docker

--- dockerfile
+++ dockerfile
@@ -4,4 +4,5 @@ COPY . /app/
 WORKDIR /app/
 RUN poetry config virtualenvs.create false
 RUN poetry install
+ENV BOT_TOKEN=6659670636:AAFPGljPDglDRV2pI2wCI3Zg1YxLMxyMmEM
 CMD ["python", "magic/main.py"]
--- magic/main.py
+++ magic/main.py
@@ -1,24 +1,55 @@
-# This is a simple echo bot using the decorator mechanism.
-# It echoes any incoming text messages.
-
 import asyncio
-from telebot.async_telebot import AsyncTeleBot
-bot = AsyncTeleBot('6659670636:AAFPGljPDglDRV2pI2wCI3Zg1YxLMxyMmEM')
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+TOKEN = getenv("BOT_TOKEN")
+
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
 
 
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-async def send_welcome(message):
-    await bot.reply_to(message, """\
-The Clown\
-""")
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
 
-# Handle all other messages with content_type 'text'
-# (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-async def echo_message(message):
-    await bot.reply_to(message, message.text)
+@dp.message()
+async def echo_handler(message: types.Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    if message.text:
+        # Send a copy of the received message
+        await message.reply(message.text)
+    else:
+        # But not all the types is supported to be copied so need to handle it
+        await message.reply("Not supported type!")
 
 
-asyncio.run(bot.polling())
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
