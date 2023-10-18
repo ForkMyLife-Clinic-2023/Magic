ecf03affbc9dfa3d28c7e8a2eb3450049c1be758
HanBinGer <35628064+HanBinGer@users.noreply.github.com>
2023-10-18 15:42:02 +0300

    fix flake8 remarks

--- magic/main.py
+++ magic/main.py
@@ -1,9 +1,9 @@
 # This is a simple echo bot using the decorator mechanism.
 # It echoes any incoming text messages.
 
+import asyncio
 from telebot.async_telebot import AsyncTeleBot
 bot = AsyncTeleBot('6659670636:AAFPGljPDglDRV2pI2wCI3Zg1YxLMxyMmEM')
-
 
 
 # Handle '/start' and '/help'
@@ -14,11 +14,11 @@ The Clown\
 """)
 
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+# Handle all other messages with content_type 'text'
+# (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 async def echo_message(message):
     await bot.reply_to(message, message.text)
 
 
-import asyncio
 asyncio.run(bot.polling())
