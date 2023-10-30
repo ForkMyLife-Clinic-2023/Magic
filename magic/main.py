97ca92acfef30ff828e3f34eed0ea8e646dacd9a
HanBinGer <elafom3@yandex.ru>
2023-10-30 16:17:36 +0300

    feat: #27: add company selection

--- magic/main.py
+++ magic/main.py
@@ -5,13 +5,42 @@ from os import getenv
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import Command, CommandStart
+from aiogram.filters.callback_data import CallbackData
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.utils.markdown import hbold
 
 TOKEN = getenv("BOT_TOKEN")
 
 dp = Dispatcher()
+
+
+class OperatingBot(StatesGroup):
+    chose_company = State()
+
+
+class CompaniesCallbackFactory(CallbackData, prefix="company"):
+    name: str
+
+
+def get_keyboard():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Рога и копыта",
+        callback_data=CompaniesCallbackFactory(name="Рога и копыта"),
+    )
+    builder.button(
+        text="Биба и Боба", callback_data=CompaniesCallbackFactory(name="Биба и Боба")
+    )
+    builder.button(
+        text="Лёлик и Болик",
+        callback_data=CompaniesCallbackFactory(name="Лёлик и Болик"),
+    )
+    builder.adjust(3)
+    return builder.as_markup()
 
 
 @dp.message(CommandStart())
@@ -21,20 +50,55 @@ async def command_start_handler(message: Message) -> None:
     Args:
         message (Message): messages with `/start` command
     """
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+    await message.answer(
+        f"Hello, {hbold(message.from_user.full_name)}!\nTo open the menu, type /menu"
+    )
+
+
+@dp.message(Command("menu"))
+async def command_menu_handler(message: Message) -> None:
+    """This handler receives messages with `/menu` command
+    and writes a message with choice of company
+
+    Args:
+        message (Message): messages with `/menu` command
+    """
+    await message.answer("Choose a company:", reply_markup=get_keyboard())
+
+
+@dp.callback_query(CompaniesCallbackFactory.filter())
+async def callbacks_switch_domen(
+    callback: types.CallbackQuery,
+    callback_data: CompaniesCallbackFactory,
+    state: FSMContext,
+):
+    match callback_data.name:
+        case "Рога и копыта":
+            await callback.message.answer("switching domen to 'Рога и копыта'")
+            await state.update_data(chosen_company="Рога и копыта")
+        case "Биба и Боба":
+            await callback.message.answer("switching domen to 'Биба и Боба'")
+            await state.update_data(chosen_company="Биба и Боба")
+        case "Лёлик и Болик":
+            await callback.message.answer("switching domen to 'Лёлик и Болик'")
+            await state.update_data(chosen_company="Лёлик и Болик")
+    await state.set_state(OperatingBot.chose_company)
+    await callback.answer()
 
 
 @dp.message()
-async def echo_handler(message: types.Message) -> None:
-    """Handler will forward receive a message back to the sender if message is text
+async def echo_handler(message: types.Message, state: FSMContext) -> None:
+    """Handler will forward receive a message back to the sender
 
     Args:
         message (types.Message): Any type of message
     """
-    if message.text:
-        await message.reply(message.text)
+    cur_state = await state.get_state()
+    if cur_state == "OperatingBot:chose_company":
+        data = await state.get_data()
+        await message.reply("{}: {}".format(data["chosen_company"], message.text))
     else:
-        await message.reply("Not supported type!")
+        await message.reply(message.text)
 
 
 async def main() -> None:
